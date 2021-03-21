46b738b96bc8668d7d048e19df6b965eb212bdd7
leila bahta <overflowngrace@gmail.com>
2021-03-20 21:42:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import Users from "./Users";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+    this.initialize = this.initialize.bind(this);
+  }
+  initialize() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.setState({ users: response.data });
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
+  componentDidMount() {
+    this.initialize();
+  }
+  render() {
+    let elements = this.state.users.map((el) => (
+      <Users id={el.id} userId={el.userId} title={el.title} />
+    ));
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>body</th>
+          </tr>
+          <tbody>{elements}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,13 @@
+import "./App.css";
+function Users(props) {
+  return (
+    <div>
+      <tr>
+        <th>{props.id}</th>
+        <th>{props.userId}</th>
+        <th>{props.title}</th>
+      </tr>
+    </div>
+  );
+}
+export default Users;
